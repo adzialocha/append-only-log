7788a82e5edd02b5a59a70f3c473df3243af3f23
Andreas Dzialocha <kontakt@andreasdzialocha.com>
2019-05-20 21:36:00 +0200

    Add backlinks and sequence numbers

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,42 @@ use ed25519_dalek::{Keypair, PublicKey, Signature};
 
 mod crypto;
 
+const HASH_LENGTH: usize = 64;
+
+fn entry_as_bytes(
+    backlink: &Option<Vec<u8>>,
+    data: &[u8],
+    sequence_number: usize,
+) -> Vec<u8> {
+    let mut bytes = Vec::new();
+
+    match backlink {
+        Some(link) => {
+            bytes.append(&mut link.clone())
+        },
+        None => {},
+    }
+
+    bytes.append(&mut data.to_vec());
+    bytes.append(&mut sequence_number.to_be_bytes().to_vec());
+
+    bytes
+}
+
+fn hash_entry(public_key: &PublicKey, entry: &LogEntry) -> Vec<u8> {
+    let bytes = entry_as_bytes(
+        &entry.backlink,
+        &entry.data,
+        entry.sequence_number
+    );
+
+    crypto::hash_data(public_key.as_bytes(), &bytes, HASH_LENGTH).as_bytes().to_vec()
+}
+
 struct LogEntry {
+    backlink: Option<Vec<u8>>,
     data: Vec<u8>,
+    sequence_number: usize,
     signature: Signature,
 }
 
@@ -21,14 +55,38 @@ impl Log {
     }
 
     pub fn append(&mut self, data: &[u8]) {
+        let sequence_number = self.len() + 1;
+
+        let backlink = match sequence_number {
+            1 => None,
+            _ => {
+                let previous_entry = self.entries.get( sequence_number - 2).unwrap();
+
+                let hashed_entry = hash_entry(
+                    &self.keypair.public,
+                    previous_entry
+                );
+
+                Some(hashed_entry)
+            },
+        };
+
+        let encoded = entry_as_bytes(
+            &backlink,
+            &data,
+            sequence_number,
+        );
+
         let signature = crypto::sign_data(
             &self.keypair.public,
             &self.keypair.secret,
-            &data
+            &encoded,
         );
 
         self.entries.push(LogEntry {
+            backlink,
             data: data.to_vec(),
+            sequence_number,
             signature,
         });
     }
@@ -48,10 +106,41 @@ impl Log {
         &self,
         public_key: &PublicKey,
     ) -> std::result::Result<(), ()> {
+        let mut sequence_number = 0;
+
         let has_invalid_entries = self.entries.iter().any(|entry| {
+            let backlink = entry.backlink.clone();
+
+            let encoded = entry_as_bytes(
+                &entry.backlink,
+                &entry.data,
+                entry.sequence_number,
+            );
+
+            // Verify backlink
+            if sequence_number > 0 {
+                let previous_entry = self.entries.get(sequence_number - 1).unwrap();
+
+                let hashed_entry = hash_entry(
+                    &self.keypair.public,
+                    previous_entry
+                );
+
+                if backlink.unwrap() != hashed_entry {
+                    return true
+                }
+            }
+
+            // Verify sequence number
+            sequence_number += 1;
+            if sequence_number != entry.sequence_number {
+                return true
+            }
+
+            // Verify signature
             crypto::verify_data(
                 &public_key,
-                &entry.data,
+                &encoded,
                 &entry.signature
             ).is_err()
         });
